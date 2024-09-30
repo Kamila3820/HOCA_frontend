e1978d7281f7aa50b3b8ab238cf9c2d91d08803d
Jiblek15 <kawiphop15@gmail.com>
2024-09-30 15:22:18 +0700

    07_commit
    replace the new HomePage to old one

--- hoca_frontend/lib/pages/home.dart
+++ hoca_frontend/lib/pages/home.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hoca_frontend/components/home/home_components.dart';
+import 'package:hoca_frontend/pages/createpost.dart';
+import 'package:hoca_frontend/pages/history.dart';
+import 'package:hoca_frontend/pages/notification.dart';
+import 'package:hoca_frontend/pages/profile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -30,154 +37,120 @@ class HomePage extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Icon(Icons.location_on),
-                    SizedBox(width: 10),
-                    Text('Choose Your Location'),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios, size: 16),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: RichText(
+                            text: TextSpan(
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontFamily: 'Poppins',
+                                ),
+                              ),
+                              children: [
+                                const TextSpan(text: 'Welcome!, '),
+                                TextSpan(
+                                  text: 'Suphanut',
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: const SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: FaIcon(
+                          FontAwesomeIcons.bell,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          size: 35,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const NotiPage()), // Navigate to NotiPage
+                        );
+                      },
+                    ),
                   ],
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Service',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildServiceOption('Cleaning', Icons.cleaning_services),
-                _buildServiceOption('Clothes', Icons.local_laundry_service),
-                _buildServiceOption('Pets', Icons.pets),
-                _buildServiceOption('Gardening', Icons.grass),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Worker Available',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 0.75,
-                ),
-                itemCount: 4, // Number of workers
-                itemBuilder: (context, index) {
-                  return _buildWorkerCard();
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.task_alt),
-            label: 'Progress',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: 0,
-        onTap: (index) {
-          // Handle bottom navigation taps
-        },
-      ),
-    );
-  }
-
-  Widget _buildServiceOption(String title, IconData icon) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Icon(icon, size: 30),
-        ),
-        const SizedBox(height: 5),
-        Text(title),
-      ],
-    );
-  }
-
-  Widget _buildWorkerCard() {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius:
-                    const BorderRadius.vertical(top: Radius.circular(10)),
-                child: Image.network(
-                  'https://via.placeholder.com/150', // Placeholder for worker's image
-                  height: 120,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                top: 10,
-                left: 10,
-                child: Container(
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: const Text(
-                    'Verified',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Teerasil Dangda',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                const SizedBox(height: 5),
-                const Text('Male, 2.4 km'),
-                const SizedBox(height: 5),
-                const Text('House/Dormitory room'),
                 const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    // Handle location selection
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Color(0xFF39A7FF),
+                            size: 26,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            'Choose Your Location',
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black.withOpacity(0.4),
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          const Icon(Icons.arrow_forward_ios, size: 18),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    'Service',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    buildServiceOption('Cleaning', FontAwesomeIcons.broom),
+                    buildServiceOption('Clothes', FontAwesomeIcons.shirt),
+                    buildServiceOption('Pets', Icons.pets),
+                    buildServiceOption('Gardening', FontAwesomeIcons.seedling),
+                  ],
+                ),
+                const SizedBox(height: 65),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
