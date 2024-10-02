4fb33957a87c44a4c43782433af9426ccd1bfc73
Puvadet   Niyomdaychar <98137581+peapablo@users.noreply.github.com>
2024-10-02 10:27:59 +0700

    fix location as same as figma

--- hoca_frontend/lib/main.dart
+++ hoca_frontend/lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import the dotenv package
+import 'pages/login.dart'; // Import your login page
 
-import 'pages/login.dart';
+Future<void> main() async {
+  // Ensure Flutter is initialized before loading environment variables
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Load environment variables from the .env file located in the assets folder
+  await dotenv.load(fileName: "assets/.env");
 
-void main() => runApp(const HOCAApp());
+  // Run the app
+  runApp(const HOCAApp());
+}
 
 class HOCAApp extends StatelessWidget {
   const HOCAApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'HOCA', // Title of the app
       debugShowCheckedModeBanner: false, // Remove the debug banner
-      home: LoginPage(), // Start with the login page
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Primary color for the app
+      ),
+      home: const LoginPage(), // Start with the login page
     );
   }
 }
--- hoca_frontend/lib/pages/locatelocation.dart
+++ hoca_frontend/lib/pages/locatelocation.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +16,8 @@ class MyApp extends StatelessWidget {
 }
 
 class LocationPage extends StatefulWidget {
+  const LocationPage({super.key});
+
   @override
   _LocationPageState createState() => _LocationPageState();
 }
